cb34df4c7fb1c1f08a0ab319ba1106cb3f39f327
Muhammad Rizki Fajar Irfani <74435307+dvvnFrtn@users.noreply.github.com>
2023-06-29 12:34:50 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+    $(".slider-1").owlCarousel({
+        nav: true,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            600: {
+                items: 1,
+            },
+            1000: {
+                items: 1,
+            },
+        },
+    });
+    $(".slider-2").owlCarousel({
+        nav: true,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            600: {
+                items: 1,
+            },
+            1000: {
+                items: 3,
+            },
+        },
+    });
+});
